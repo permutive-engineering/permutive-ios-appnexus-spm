5d12a1a9dbbbecdea9ee07fa477a929ac4509807
Chris Gibson <chrisg_aust@yahoo.com>
2022-07-25 16:25:49 +0100

    Initial commit v1

---
+++ Package.swift
@@ -0,0 +1,31 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Permutive_iOS_AppNexus",
+    platforms: [.iOS(.v11)],
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "Permutive_iOS_AppNexus",
+            targets: ["Permutive_iOS_AppNexus"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        .package(
+            url: "https://github.com/permutive-engineering/permutive-ios-spm.git",
+            from: "1.5.0"
+        ),
+        .package(
+            url: "https://github.com/appnexus/mobile-sdk-ios.git",
+            .exact("7.22.0")
+        )
+    ],
+    targets: [
+        .binaryTarget(name:"Permutive_iOS_AppNexus",
+		url:"https://storage.googleapis.com/permutive-ios-sdks/swift-sdk/Permutive-iOS-AppNexus-v1.0.0.zip",
+		checksum:"7bf3dd63854fe60b1a6dce86e83475fb02db50ff6706dc5834534e0e75fb4913")
+    ]
+)
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# permutive-ios-appnexus-spm
-Swift Package Manager repository for Permutive's AppNexus framework
+# Permutive iOS AppNexus SDK Swift Package
+
+This provides a Swift Package for Permutive iOS AppNexus SDK and offers improved tracking support.
+
+Please refer to the SDK documentation [here](https://developer.permutive.com/docs/ios).
+Get the latest release notes [here](https://developer.permutive.com/docs/ios-release-notes).
