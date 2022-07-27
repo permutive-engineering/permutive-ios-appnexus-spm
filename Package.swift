f867a5a4e16ad761d7ac088129b252349e241bb5
Chris Gibson <chrisg_aust@yahoo.com>
2022-07-27 19:03:30 +0100

    Release v1.0.0

--- Package.swift
+++ Package.swift
@@ -26,6 +26,6 @@ let package = Package(
     targets: [
         .binaryTarget(name:"Permutive_iOS_AppNexus",
 		url:"https://storage.googleapis.com/permutive-ios-sdks/swift-sdk/Permutive_iOS_AppNexus-v1.0.0.zip",
-		checksum:"7bf3dd63854fe60b1a6dce86e83475fb02db50ff6706dc5834534e0e75fb4913")
+		checksum:"11a137d58434309d3ff6420112d34b36f750c3a534bc5dac9705ab6f25f1e74e")
     ]
 )
